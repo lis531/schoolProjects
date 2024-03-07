5f6d3737cea226e66a7df6284148eb728579e7f0
lis531 <82408934+lis531@users.noreply.github.com>
2024-03-07 23:51:49 +0100

    Mid update
    fixed shopList, changed #list from flex to grid in countriesAPI, some other changes

--- shopList/script.js
+++ shopList/script.js
@@ -20,7 +20,7 @@ const updateUI = () => {
 const createButton = () => {
     const button = document.createElement("button");
     button.classList.add("btn-link");
-    button.innerHTML = `<svg fill="red" viewBox="0 0 448 512" height="15px" width="15px" xmlns="http://www.w3.org/2000/svg" style="rotate: 45deg;"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"></path></svg>`
+    button.innerHTML = `<svg fill="red" viewBox="0 0 448 512" height="15px" width="15px" style="rotate: 45deg;"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"></path></svg>`
     return button;
 }
 
@@ -44,6 +44,7 @@ const addItem = (event) => {
 
 const removeItem = (item) => {
     if(confirm("Are you sure?")){
+        console.log(item);
         item.remove();
         updateUI();
     }
@@ -105,11 +106,6 @@ const filtr = () => {
         }
     })
 }
-
-for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", () => removeItem(buttons[i].parentElement));
-}
-
 
 myForm.addEventListener("submit", addItem);
 
